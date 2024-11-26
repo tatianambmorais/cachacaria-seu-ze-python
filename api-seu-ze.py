9b587ac289bf928c62d9a95989ff49b8f6350560
Luís Eduardo <luisedunascimento17@gmail.com>
2024-11-26 11:21:53 -0300

    Atualizando função de traduzir, arquivo PY e JS

--- api-seu-ze.py
+++ api-seu-ze.py
@@ -7,11 +7,18 @@ app = Flask(__name__)
 CORS(app) 
 
 
-def translate(texts):
+def translate(conselhos):
     "Função para traduzir texto"
     try:
-        texto_processado = [GoogleTranslator(source='en', target='pt').translate(texto) for texto in texts]
-        return texto_processado
+        # introduz a variável que guardará os objetos
+        data = []
+        for conselho in conselhos:
+            #traduz o texto
+            traduzido = GoogleTranslator(source='en', target='pt').translate(conselho['advice'])
+            # passa o ID e o advice, de modo que siga o mesmo padrão do GET original da API, para não conflitar com o código principal
+            conselhoObjetoTraduzido = {"id": conselho["id"], "advice": traduzido}
+            data.append(conselhoObjetoTraduzido)
+        return data
     except Exception as e:
         return f"Erro encontrado: {e}"
 
@@ -37,7 +44,7 @@ def obter_conselho():
         response = requests.get("https://api.adviceslip.com/advice")
         if response.status_code == 200:
             data = response.json()
-            return data['slip']['advice']
+            return data['slip']
         else:
             return "Não foi possível obter um conselho no momento. Tente novamente mais tarde."
     except Exception as e:
